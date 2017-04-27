2fb821f5d2df81c038b5c60c342b18003ffd8cb3
psalv <psalvato@uwo.ca>
2017-04-26 23:34:49 -0400

    minor fix

--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Josiah's Journey</title>
+    <title>Blank Heroku Project</title>
     <!-- Favicons
     ================================================== -->
     <link rel="manifest" href="assets/img/favicon/manifest.json">
